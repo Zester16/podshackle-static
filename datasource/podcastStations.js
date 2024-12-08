08bbc2892a9c97f47c22fb45e76b3a510ef7ecf3
mharsh15 <zesterschmid16@gmail.com>
2024-12-08 17:53:04 +0530

    v_0_0_8A: add new stations, add new background color, add image padding

--- datasource/podcastStations.js
+++ datasource/podcastStations.js
@@ -11,4 +11,11 @@ const podcastStations = [
     logo: "https://image.simplecastcdn.com/images/48d244e8-5815-45c1-9b60-b03aceaa2ad5/764215f2-3998-47c4-8a24-8c56dffe623f/3000x3000/bx22354-podcast-tiles-rebrand-series-inside-economics.jpg?aid=rss_feed",
     url: "https://feeds.simplecast.com/4LZRim3c",
   },
+  {
+    id: "reuters_econ_world",
+    name: "Reuters Econ World",
+    logo: "https://megaphone.imgix.net/podcasts/3c37d102-0c70-11ef-9f2a-d3c80df1fc79/image/e23a1724ac8f5dc3f1fdc335e97c6222.jpg?ixlib=rails-4.3.1&max-w=3000&max-h=3000&fit=crop&auto=format,compress",
+    url: "https://feeds.megaphone.fm/reuterseconworld",
+  },
 ];
+//https://feeds.megaphone.fm/reuterseconworld
